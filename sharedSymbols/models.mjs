08393f60cfa1a59b0262d2bf1580f9fae793dae7
Gurden <gurdensingh@gmail.com>
2021-04-03 18:37:51 -0700

    major push on server side

--- sharedSymbols/models.mjs
+++ sharedSymbols/models.mjs
@@ -27,3 +27,14 @@ export function ListItem(description, id=null){
     this.description = description;
     this.id = id;
 }
+
+/**
+ * An endpoint Model
+ * @param {string} name - includes the method and path of the endpoint
+ * @param {Number} count - The number of times an endpoint has been hit
+ * @constructor
+ */
+export function Endpoint(name, count) {
+    this.name = name;
+    this.count = count;
+}
